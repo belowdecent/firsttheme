4e7f3d6ac42cbc357ed583c99b58a452e134badd
Askar Kanturin <a.kanturin@yandex.ru>
2023-08-05 19:54:30 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+/** 
+ * Main template file
+ * 
+ * @package firsttheme
+ */
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Hello</title>
+</head>
+
+<body>
+	Hi
+</body>
+</html>
